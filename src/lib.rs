41842e4f8ec19e1a61dd915bec9d3495a0576a41
capJavert <ante@kickass.website>
2022-12-01 15:33:21 +0100

    feat(day-1): part 2 wasm

--- src/lib.rs
+++ src/lib.rs
@@ -28,3 +28,21 @@ macro_rules! console_log {
 pub fn merry_xmas(year: Option<i32>) {
     console_log!("Merry Xmas {}!", year.unwrap_or(2022))
 }
+
+#[wasm_bindgen]
+pub fn solve_day_1(input: Vec<i32>) -> i32 {
+    let mut elfs = input.iter().fold(vec![0], |mut acc, item| {
+        if item.eq(&0) {
+            acc.push(0)
+        } else {
+            let elf = acc.last_mut().unwrap();
+            *elf += *item;
+        }
+
+        acc
+    });
+
+    elfs.sort_by(|a, b| b.cmp(a));
+
+    elfs[0..3].to_vec().iter().sum::<i32>()
+}
