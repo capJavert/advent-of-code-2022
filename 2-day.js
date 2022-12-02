2cfa1bc80d9e374221ecb548be8873afe9fbdd62
capJavert <ante@kickass.website>
2022-12-02 15:01:06 +0100

    feat(day-2): part 2

--- 2-day.js
+++ 2-day.js
@@ -4,19 +4,25 @@ const { solve_day_2 } = require('./pkg')
 const main = async () => {
     const data = await fetch('https://pastebin.com/raw/t5tJzLRL').then(response => response.text())
     const input = data.split(/\r?\n/)
+    const choices = ['Rock', 'Paper', 'Scissors']
 
     const rounds = input.map(line => {
-        return line.split(' ').map(
-            item =>
-                ({
-                    A: 'Rock',
-                    B: 'Paper',
-                    C: 'Scissors',
-                    X: 'Rock',
-                    Y: 'Paper',
-                    Z: 'Scissors'
-                }[item])
-        )
+        const [a, b] = line.split(' ')
+        const him = {
+            A: 'Rock',
+            B: 'Paper',
+            C: 'Scissors'
+        }[a]
+
+        const modifier = {
+            X: 2,
+            Y: 0,
+            Z: 1
+        }[b]
+        const choice = (choices.findIndex(item => item === him) + modifier) % 3
+        const me = choices[choice]
+
+        return [him, me]
     })
 
     console.log(solve_day_2(rounds))
