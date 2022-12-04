886e8314aa4d42f76de1fde5799a117f3d2dbc57
capJavert <ante@kickass.website>
2022-12-04 13:06:06 +0100

    feat(day-4): part 2

--- src/lib.rs
+++ src/lib.rs
@@ -122,13 +122,15 @@ pub fn solve_day_3(input: JsValue) -> usize {
 pub fn solve_day_4(input: JsValue) -> usize {
     let elf_pairs: Vec<Vec<Vec<usize>>> = serde_wasm_bindgen::from_value(input).unwrap();
 
+    let is_range_overlap = |range1: &Vec<usize>, range2: &Vec<usize>| -> bool {
+        range1.first() <= range2.last() && range2.first() <= range1.last()
+    };
+
     let inefficient_pair_count = elf_pairs.into_iter().fold(0, |acc, pair| {
         let range1 = &pair[0];
         let range2 = &pair[1];
 
-        if range1.first() <= range2.first() && range1.last() >= range2.last() {
-            acc + 1
-        } else if range2.first() <= range1.first() && range2.last() >= range1.last() {
+        if is_range_overlap(range1, range2) {
             acc + 1
         } else {
             acc
