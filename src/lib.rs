d6e93c46bc8b98b0e8030ca23360f89259ca2f48
capJavert <ante@kickass.website>
2022-12-13 19:14:13 +0100

    refactor: move rust files to solutions
    feat: autoload new solutions as files are added

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,11 @@
 use wasm_bindgen::prelude::*;
 
-pub mod day_1;
-pub mod day_10;
-pub mod day_11;
-pub mod day_12;
-pub mod day_13;
-pub mod day_2;
-pub mod day_3;
-pub mod day_4;
-pub mod day_5;
-pub mod day_6;
-pub mod day_7;
-pub mod day_8;
-pub mod day_9;
 pub mod utils;
+
+#[allow(dead_code)]
+pub mod solutions {
+    automod::dir!("src/solutions");
+}
 
 #[wasm_bindgen]
 pub fn merry_xmas(year: Option<i32>) {
